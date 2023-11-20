f5eac6261a02b5be1c18be9373e4e2d355f84bc5
Pedro Mota Cassemiro <107152636+Pemoca@users.noreply.github.com>
2023-11-19 22:00:28 -0300

    Ajuste ScrollView tela perfil

--- src/cooklator/components/Profile.js
+++ src/cooklator/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import { Button } from 'react-native-paper';
 import { TextInput } from 'react-native-paper';
 import { Checkbox } from 'react-native-paper';
@@ -99,111 +99,112 @@ const Profile = () => {
 
     return (
 
-        <View style={{paddingTop: 16}}>
-            <Card style={{paddingBottom: 16, marginHorizontal: 8}} elevation={3} >
-                <View style={styles.inputContainer}>
+        <ScrollView>
+            <View style={{paddingTop: 16}}>
+                <Card style={{paddingBottom: 16, marginHorizontal: 8}} elevation={3} >
+                    <View style={styles.inputContainer}>
 
-                    <Text style={{fontSize: 18, fontWeight:'bold', paddingTop: 16}}>
-                        Perfil
-                    </Text>
+                        <Text style={{fontSize: 18, fontWeight:'bold', paddingTop: 16}}>
+                            Perfil
+                        </Text>
 
-                    <TextInput
-                        style={styles.input}
-                        outlineColor={isNameValid ? 'gray' : 'red'}
-                        label="Nome"
-                        value={name}
-                        mode='outlined'
-                        onChange={nameChange}
-                    />
-                    {!isNameValid && <Text style={styles.invalidInput}>O nome deve conter entre 5 e 15 caracteres </Text>}
-                    <TextInput
-                        style={styles.input}
-                        outlineColor={isEmailValid ? 'gray' : 'red'}
-                        label= "E-mail"
-                        value={email}
-                        mode='outlined'
-                        onChange={emailChange}
-                        errorMessage={"Teste"}
-                    />
-                    {!isEmailValid && <Text style={styles.invalidInput}>Endereço de E-mail inválido</Text>}
-
-                    <View style={styles.input}>
                         <TextInput
-                            style={{width: '100%', paddingBottom: 0}}
-                            label="Valor por Hora"
-                            value={valueHour}
+                            style={styles.input}
+                            outlineColor={isNameValid ? 'gray' : 'red'}
+                            label="Nome"
+                            value={name}
                             mode='outlined'
-                            onChange={valueHourChange}
+                            onChange={nameChange}
                         />
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Text style={{fontWeight: 'bold', fontSize: 12}}>Fixar para todos os projetos</Text>
-                            <Checkbox
-                                status={checked ? 'checked' : 'unchecked'}
-                                onPress={() => {
-                                    setChecked(!checked);
-                                }}
+                        {!isNameValid && <Text style={styles.invalidInput}>O nome deve conter entre 5 e 15 caracteres </Text>}
+                        <TextInput
+                            style={styles.input}
+                            outlineColor={isEmailValid ? 'gray' : 'red'}
+                            label= "E-mail"
+                            value={email}
+                            mode='outlined'
+                            onChange={emailChange}
+                            errorMessage={"Teste"}
+                        />
+                        {!isEmailValid && <Text style={styles.invalidInput}>Endereço de E-mail inválido</Text>}
+
+                        <View style={styles.input}>
+                            <TextInput
+                                style={{width: '100%', paddingBottom: 0}}
+                                label="Valor por Hora"
+                                value={valueHour}
+                                mode='outlined'
+                                onChange={valueHourChange}
                             />
+                            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                <Text style={{fontWeight: 'bold', fontSize: 12}}>Fixar para todos os projetos</Text>
+                                <Checkbox
+                                    status={checked ? 'checked' : 'unchecked'}
+                                    onPress={() => {
+                                        setChecked(!checked);
+                                    }}
+                                />
+                            </View>
                         </View>
+
+                        <Button icon="square-edit-outline" mode="contained" onPress={() => console.log('Pressed')}>
+                            Editar
+                        </Button>
+
                     </View>
+                </Card>
 
-                    <Button icon="square-edit-outline" mode="contained" onPress={() => console.log('Pressed')}>
-                        Editar
-                    </Button>
+                <Card style={{paddingBottom: 16, marginHorizontal: 8, marginVertical: 20}} elevation={3} >
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.input}
+                            label="Senha Atual"
+                            value={currentPassword}
+                            mode='outlined'
+                            onChange={currentPasswordChange}
+                            right={<TextInput.Icon icon={hideCurrentPassword ? "eye-off" : "eye"} onPress={hideCurrentPasswordChange}/>}
+                            secureTextEntry={hideCurrentPassword}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            label="Nova Senha"
+                            value={newPassword}
+                            outlineColor={isNewPasswordValid ? 'gray' : 'red'}
+                            mode='outlined'
+                            onChange={newPasswordChange}
+                            right={<TextInput.Icon icon={hideNewPassword ? "eye-off" : "eye"} onPress={hideNewPasswordChange}/>}
+                            secureTextEntry={hideNewPassword}
+                        />
+                        {!isNewPasswordValid && <Text style={styles.invalidInput}>A nova senha deve conter 8 caracteres</Text>}
+                        <TextInput
+                            style={styles.input}
+                            label="Confirmação Nova Senha"
+                            value={newPasswordConfirmation}
+                            outlineColor={isNewPasswordConfirmationValid ? 'gray' : 'red'}
+                            mode='outlined'
+                            onChange={newPasswordConfirmationChange}
+                            right={<TextInput.Icon icon={hideNewPasswordConfirmation ? "eye-off" : "eye"} onPress={hideNewPasswordConfirmationChange}/>}
+                            secureTextEntry={hideNewPasswordConfirmation}
+                        />
+                        {!isNewPasswordConfirmationValid && <Text style={styles.invalidInput}>Confirmação de senha inválida</Text>}
 
-                </View>
-            </Card>
+                        <Button
+                            style={{marginVertical: 10}}
+                            icon="square-edit-outline"
+                            mode="contained"
+                            onPress={() => console.log('Pressed')}>
+                            Alterar Senha
+                        </Button>
 
-            <Card style={{paddingBottom: 16, marginHorizontal: 8, marginVertical: 20}} elevation={3} >
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        label="Senha Atual"
-                        value={currentPassword}
-                        mode='outlined'
-                        onChange={currentPasswordChange}
-                        right={<TextInput.Icon icon={hideCurrentPassword ? "eye-off" : "eye"} onPress={hideCurrentPasswordChange}/>}
-                        secureTextEntry={hideCurrentPassword}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        label="Nova Senha"
-                        value={newPassword}
-                        outlineColor={isNewPasswordValid ? 'gray' : 'red'}
-                        mode='outlined'
-                        onChange={newPasswordChange}
-                        right={<TextInput.Icon icon={hideNewPassword ? "eye-off" : "eye"} onPress={hideNewPasswordChange}/>}
-                        secureTextEntry={hideNewPassword}
-                    />
-                    {!isNewPasswordValid && <Text style={styles.invalidInput}>A nova senha deve conter 8 caracteres</Text>}
-                    <TextInput
-                        style={styles.input}
-                        label="Confirmação Nova Senha"
-                        value={newPasswordConfirmation}
-                        outlineColor={isNewPasswordConfirmationValid ? 'gray' : 'red'}
-                        mode='outlined'
-                        onChange={newPasswordConfirmationChange}
-                        right={<TextInput.Icon icon={hideNewPasswordConfirmation ? "eye-off" : "eye"} onPress={hideNewPasswordConfirmationChange}/>}
-                        secureTextEntry={hideNewPasswordConfirmation}
-                    />
-                    {!isNewPasswordConfirmationValid && <Text style={styles.invalidInput}>Confirmação de senha inválida</Text>}
+                    </View>
+                </Card>
 
-                    <Button
-                        style={{marginVertical: 10}}
-                        icon="square-edit-outline"
-                        mode="contained"
-                        onPress={() => console.log('Pressed')}>
-                        Alterar Senha
-                    </Button>
+                {/*<SafeAreaProvider>*/}
+                {/*    <AppBar />*/}
+                {/*</SafeAreaProvider>*/}
 
-                </View>
-            </Card>
-
-            {/*<SafeAreaProvider>*/}
-            {/*    <AppBar />*/}
-            {/*</SafeAreaProvider>*/}
-
-        </View>
-
+            </View>
+        </ScrollView>
     );
 
 }
